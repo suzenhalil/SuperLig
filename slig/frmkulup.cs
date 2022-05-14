9bc4954f4573f5dcd44f329cd21466940a379da4
suzenhalil <suzenhalil@hotmail.com>
2022-05-14 22:49:22 +0300

    Add project files.

---
+++ slig/frmkulup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slig
+{
+    public partial class frmkulup : Form
+    {
+        public frmkulup()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "1940 yılında, 2. Dünya Savaşı’nın yaşandığı zamanlarda, silah altında bulunan askerlerin dışında kalan gençleri, sivil savunma ve spora yöneltmek amacıyla ‘Sivill Savunma Mükellefiyeti’ adı altında çıkan kanun sonucunda ‘Kamu ve Özel Sektörde 500 Kişiden Fazla Eleman Çalıştıran Kuruluşların Spor Kulübü Kurmaları Mecburiyeti’ neticesinde, 21 Aralık 1940 tarihinde, o zamanın Devlet Demiryolları 6. İşletme Müdürü Eşref Demirağ tarafından Adana Demirspor Kulübü kurulmuş, resmi tescilini bir hafta sonra 28 Aralık 1940 tarihinde almıştır.";
+            pictureBox1.ImageLocation = @"C:\Users\suzen\source\repos\calismaca\slig\bin\Debug\armalar\adn.png";
+            textBox1.Text = "VINCENZO MONTELLA";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "İkinci Meşrutiyet (1908)’e kadar Türkiye’de spor yapmak hem padişah yönetiminin baskısı, hem de muhafazakarların tutumu nedeniyle hemen hemen olanaksız gibiydi. Spor yapanlar o dönemde pederşahi bir zihniyetle ayıplanırdı. Türkiye’ de modern beden eğitimi öncüsü Selim Sırrı Tarcan, 1919 yılında sporu geliştirmek amacıyla bir salon açmak için İzmir’e geldi. Onun bu girişimi Sarıklılar olarak tabir edilen aşırı muhafazakarlar tarafından engellendi. Selim Sırrı Tarcan, salon açamamasına rağmen, o dönemde Vali Rahmi Bey, Necati Bey ve Vasıf Çınar Bey’le görüştü. Tarcan’ın spor sevgisi aşısı sonucu Rum ve Ermeniler ile diğer azınlığın da etkinliği nedeniyle artık Türk gençleri spor yapma gereğini duymaya başladı. Türkiye’de futbol ilk kez Rum, Ermeni, İngiliz ve İtalyanlar tarafından 1898 yılında oynandı. 1905 yılında ise Amerikan Koleji’nde eğitim gören Talat Erboy, orada okuyan iki arkadaşı Şerif Remzi Reyent ve Sabri Süleymanoviç’i de yanına alarak yabancı öğrencilerle birlikte futbol oynamaya başladı.";
+            pictureBox1.ImageLocation = @"C:\Users\suzen\source\repos\calismaca\slig\bin\Debug\armalar\altay.png";
+            textBox1.Text = "MUSTAFA DENİZLİ";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "1967 yılında müsabakalarla halkın ilgisini çekmek ve gençlerin kötü alışkanlıklarından kurtulması için şehrin ileri gelenleri tarafından kurulmuştur.Renkleri Bordo- Beyazdır. Ayrıca Defneyaprağı Yeşili Renginide üçüncü renk olarak almıştır. Bordo Rengi Asaleti, Beyaz Rengi Temizliği, Defneyaprağı Yeşili de Barışı Simgeler.İlk şampiyonluğu 1969-1970 sezonunda teknik direktör İlker Tolon birlikte yaşamıştır. Daha sonra aralıklarla 3 Lig’e düşmüş, tekrar 2. Lig’e çıkarak inişli çıkışlı bir grafik izlemiştir.Hatayspor bugün futbol, hentbol, masatenisi, güreş, basketbol, voleybol, yüzme, okçuluk, atletizim gibi Tescili olan branşlarda faaliyet göstermektedir. En önemli Tescili branşları ise Futbol,güreş ve atletizimdir.";
+            pictureBox1.ImageLocation = @"C:\Users\suzen\source\repos\calismaca\slig\bin\Debug\armalar\hatay.png";
+            textBox1.Text = "ÖMER ERDOĞAN";
+
+        }
+        
+        
+    }
+}
